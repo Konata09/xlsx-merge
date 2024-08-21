d2a55fa46056df8c3173a32db26b6c87a72a2bd1
Konata09 <lz9709@gmail.com>
2024-08-21 14:32:10 +0800

    add e2e test

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
+use std::str::from_utf8;
 
 #[test]
 fn test_merge() {
     // Setup: Create temporary test files
-    let source_file = " ../../tests/horegopos_mobile_202408191053.xlsx ";
-    let ref_file = "../../tests/restaurant_mobile_202408160626_2.3.xlsx";
+    let source_file = "tests/horegopos_uploader_h5_202408191055.xlsx";
+    let ref_file = "tests/restaurant_uploader_h5_202408160639_2.3.xlsx";
     let column = "印度尼西亚语";
 
     // Run the main program with test arguments
@@ -17,6 +18,8 @@ fn test_merge() {
         .output()
         .expect("Failed to execute process");
 
+    println!("{}", from_utf8(&output.stderr).expect("Failed to convert stderr to string"));
+    println!("{}", from_utf8(&output.stdout).expect("Failed to convert stdout to string"));
     // Check if the program executed successfully
     assert!(output.status.success());
 }
